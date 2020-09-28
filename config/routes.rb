cf2ff1af5a8fc6f7c3a93af775c676ee4486c8f2
Ingrid Pauchon <ingridpauchon@gmail.com>
2020-09-28 17:18:12 +0200

    changed the root route to #home and added show action to our routes in Treehouses controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  root to: 'treehouses#index'
+  root to: 'treehouses#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   # Add upon these routes as and when you need to make a view
-  resources :treehouses, only: %i[index home]
+  resources :treehouses, only: %i[home index show]
 end
