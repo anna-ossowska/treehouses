54826acb7bfaeaf57b3a5e5bbe9db3bc32dea5b7
Ingrid Pauchon <ingridpauchon@gmail.com>
2020-09-30 13:48:17 +0200

    added pundit gem and set for pundit

--- app/controllers/treehouses_controller.rb
+++ app/controllers/treehouses_controller.rb
@@ -1,10 +1,19 @@
 class TreehousesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
+
   def show
     @treehouse = Treehouse.find(params[:id])
+    @treehouses = policy_scope(Treehouse)
   end
 
   def index
     @treehouses = Treehouse.search(params)
+    @treehouses = policy_scope(Treehouse).order(created_at: :desc)
   end
+
+  def new
+    @treehouse = Treehous.new
+    authorize @treehouse
+  end
+
 end
