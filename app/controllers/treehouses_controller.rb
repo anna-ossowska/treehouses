1dbebc93e1bb84c8521c0ad82d6d2aefe5110ab5
April Yeats <yeats.april@gmail.com>
2020-09-30 12:21:38 +0200

    Bookings are enabled. You can now make a booking on each treehousen page for the current user. Right now it just redirects back to the treehouse page with no prompt to say the booking has been completed. I also left in a raise in case the booking doesn't save so you know it's broken.

--- app/controllers/treehouses_controller.rb
+++ app/controllers/treehouses_controller.rb
@@ -1,10 +1,22 @@
 class TreehousesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index, :show]
-  def show
-    @treehouse = Treehouse.find(params[:id])
-  end
+  skip_before_action :authenticate_user!, only: %i[index show]
+  before_action :set_treehouse, only: %i[new show]
 
   def index
     @treehouses = Treehouse.all
+  end
+
+  def show
+    @booking = Booking.new
+  end
+
+  def new
+    @treehouse = Treehouse.new
+  end
+
+  private
+
+  def set_treehouse
+    @treehouse = Treehouse.find(params[:id])
   end
 end
