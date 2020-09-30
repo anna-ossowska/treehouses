3746dc51fdf989b4b8dc19e481406517b5a54740
petter syvertsen <petter.syvertsen@gmail.com>
2020-09-30 14:53:38 +0200

    Styled and BE imported

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find(params[:id])
+    @user = current_user
+    @booking = @user.bookings[0]
+    t_id = Treehouse.find([@user.bookings[0].treehouse_id])
+    @treehouse = t_id[0].name
   end
 end
