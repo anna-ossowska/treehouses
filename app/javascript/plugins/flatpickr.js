6be01e0347007ef6f2ec951dab0b0e71281230d6
Ingrid Pauchon <ingridpauchon@gmail.com>
2020-10-02 16:28:47 +0200

    fixed flatpickr and date availability

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -4,10 +4,10 @@ import rangePlugin from "flatpickr/dist/plugins/rangePlugin";
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
-   flatpickr("#range_start", {
-     altInput: true,
-     plugins: [new rangePlugin({ input: "#range_end"})]
-   });
+   // flatpickr("#range_start", {
+     // altInput: true,
+     // plugins: [new rangePlugin({ input: "#range_end"})]
+   // });
   toggleDateInputs();
 });
 
@@ -22,21 +22,22 @@ const toggleDateInputs = function() {
       minDate: 'today',
       dateFormat: 'Y-m-d',
       disable: unvailableDates,
+      plugins: [new rangePlugin({ input: "#range_end"})],
       onChange: function(selectedDates, selectedDate) {
         if (selectedDate === '') {
           endDateInput.disabled = true;
         }
         let minDate = selectedDates[0];
-        minDate.setDate(minDate.getDate() + 1);
+        minDate.setDate(minDate.getDate());
         endDateCalendar.set('minDate', minDate);
         endDateInput.disabled = false;
       }
     });
-    const endDateCalendar =
-    flatpickr(endDateInput, {
-      dateFormat: 'Y-m-d',
-      disable: unvailableDates,
-    });
+    // const endDateCalendar =
+    // flatpickr(endDateInput, {
+      // dateFormat: 'Y-m-d',
+      // disable: unvailableDates,
+    // });
   }
 };
 
